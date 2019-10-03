9dbd2dae168886732900483cf44db51805d4856e
songvo1018 <52455378+songvo1018@users.noreply.github.com>
2019-10-03 06:26:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ function calculate() {
     var thickRing = parseFloat(document.getElementById('thickRing').value);
     var widthRing = parseFloat(document.getElementById('widthRing').value);
     var colorRing = document.getElementById('colorRing').value;
+    
     // Calculate
     var firstCoef = (function (sizeRing, thickRing) {
         return (sizeRing + thickRing) * 3.14;
     })(sizeRing, thickRing);
+    
     var secondCoef = (function (firstCoef, widthRing) {
         if (widthRing > 6) {
             return (firstCoef + 0.4);
@@ -19,6 +21,7 @@ function calculate() {
             return (firstCoef - 0.3);
         }
     })(firstCoef, widthRing);
+    
     var thirdCoef = (function (secondCoef, colorRing) {
         if (colorRing === 'white') {
             return secondCoef + 0.3;
@@ -28,9 +31,11 @@ function calculate() {
             return secondCoef - 0.25;
         }
     })(secondCoef, colorRing);
+    
     // Output
     document.getElementById('resultSpace').innerHTML += '<br>' + '  ||  RESULT   ' + thirdCoef.toFixed(2) + ' millimetrs  ' + ' (' + colorRing + ')   ||';
     console.log(thirdCoef.toFixed(2));
 };
+
 // start function
 document.getElementById('btn__ok').addEventListener('click', calculate);
