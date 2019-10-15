7d7976d234602b15e89a4b2cc4af5935295bba5d
songvo1018 <52455378+songvo1018@users.noreply.github.com>
2019-10-15 19:26:49 +0300

    Update script.js
    So, i get some progress in learning course of JS. And i change variable type on the 'const' and 'let', because it is many powerful practice. Plus,  now you can calculate after press any 'enter' and button OK.

--- script.js
+++ script.js
@@ -1,42 +1,52 @@
 function calculate() {
     // Input
-    var sizeRing = parseFloat(document.getElementById('sizeRing').value);
-    var thickRing = parseFloat(document.getElementById('thickRing').value);
-    var widthRing = parseFloat(document.getElementById('widthRing').value);
-    var colorRing = document.getElementById('colorRing').value;
-    
-    // Calculate
-    var firstCoef = (function (sizeRing, thickRing) {
-        return (sizeRing + thickRing) * 3.14;
-    })(sizeRing, thickRing);
-    
-    var secondCoef = (function (firstCoef, widthRing) {
-        if (widthRing > 6) {
-            return (firstCoef + 0.4);
-        } else if (widthRing > 4) {
-            return (firstCoef + 0.2);
-        } else if (widthRing < 4 && widthRing > 2) {
-            return firstCoef;
-        } else {
-            return (firstCoef - 0.3);
-        }
-    })(firstCoef, widthRing);
-    
-    var thirdCoef = (function (secondCoef, colorRing) {
-        if (colorRing === 'white') {
-            return secondCoef + 0.3;
-        } else if (colorRing === 'red') {
-            return secondCoef;
-        } else if (colorRing === 'lemon') {
-            return secondCoef - 0.25;
-        }
-    })(secondCoef, colorRing);
-    
-    // Output
-    document.getElementById('resultSpace').innerHTML += '<br>' + '  ||  RESULT   ' 
-        + thirdCoef.toFixed(2) + ' millimetrs  ' + ' (' + colorRing + ')   ||';
-    console.log(thirdCoef.toFixed(2));
-};
+    const sizeRing = parseFloat(document.getElementById('sizeRing').value);
+    const thickRing = parseFloat(document.getElementById('thickRing').value);
+    const widthRing = parseFloat(document.getElementById('widthRing').value);
+    const colorRing = document.getElementById('colorRing').value;
 
+    // Calculate
+    let sizeCoef = (function (sizeRing, thickRing) {
+        if (thickRing > 1.7) {
+            return ((sizeRing + thickRing) * 3.14) - 0.4;
+        } else {
+            return (sizeRing + thickRing) * 3.14;
+        }
+
+    })(sizeRing, thickRing);
+
+    let widthCoef = (function (sizeCoef, widthRing) {
+        if (widthRing > 6) {
+            return (sizeCoef + 0.4);
+        } else if (widthRing > 4) {
+            return (sizeCoef + 0.2);
+        } else if (widthRing < 4 && widthRing > 2) {
+            return sizeCoef;
+        } else {
+            return (sizeCoef - 0.3);
+        }
+    })(sizeCoef, widthRing);
+
+    let colorCoef = (function (widthCoef, colorRing) {
+        if (colorRing === 'white') {
+            return widthCoef + 0.3;
+        } else if (colorRing === 'red') {
+            return widthCoef;
+        } else if (colorRing === 'lemon') {
+            return widthCoef - 0.25;
+        }
+    })(widthCoef, colorRing);
+
+    // Output
+    document.getElementById('resultSpace').innerHTML += `${'<br></br>'}RESULT => ${colorCoef.toFixed(2)} millimetres (${colorRing}) ||`;
+    console.log(colorCoef.toFixed(2));
+};
 // start function
+document.getElementById('btn__ok').addEventListener('click', calculate);
+document.querySelector('.btn__ok').addEventListener('click', calculate);
+document.addEventListener('keypress', function (event) {
+    if (event.keyCode === 13 || event.which === 13) {
+        calculate();
+    }
+});
 document.getElementById('btn__ok').addEventListener('click', calculate);
